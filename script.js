28812a22773db4cc2641eced30424f5c32b1950e
Jonathon Newell <jonathonnewell123@gmail.com>
2022-12-19 18:27:52 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Write your JavaScript below this line
+var name = "Jonathon";
+var favoriteSnack = "chips";
+
+console.log(
+  "Hi, my name is " +
+    name +
+    " and I wish I had " +
+    favoriteSnack +
+    " right now. "
+);
